219a55db303ac72c853aa95d1f6c69733aadcb14
Tefo-K <tefokeabetswe4@gmail.com>
2023-07-17 15:30:35 +0100

    fix: completed variable excersies using const and let.

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -16,10 +16,12 @@ people.forEach(logPerson);
 // Function in a Variable
 // Store the logPerson() function in a variable called performLogPerson
 
-// Your code here
+const performLogPerson = logPerson();
 
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
+const arrowVersionOfLogPerson = (person) =>
+  console.log("The person is " + person);
 
 // Your code here
 
