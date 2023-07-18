84bc9e9c74dbae2258586558712fd84536463bf9
Tefo-K <tefokeabetswe4@gmail.com>
2023-07-18 16:27:31 +0100

    fix: added missing comma in test and refactored sayHello function using ternary.

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,7 +1,6 @@
 // Instructions can be found in ternaries.md
 
 export function sayHello(name) {
-  if (name === undefined) {
-    return "Hello you!";
-  } else return "Hello, " + name + "!";
+  const addName = name ? name : "you";
+  return `Hello, ${addName}!`;
 }
